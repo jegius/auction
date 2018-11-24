00adfdd695933fa3dcfeaff41be005aa87a86aeb
jegius <jegius@gmail.com>
2018-11-24 12:44:30 +0300

    Added validation for search form.

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,14 +1,39 @@
 import {Component} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {ProductService} from '../../services/product.service';
 
 @Component({
   selector: 'app-auction-search',
-  templateUrl: 'search.component.html'
+  templateUrl: 'search.component.html',
+  providers: [ProductService]
 })
 export default class SearchComponent {
-  formModel: FormGroup = new FormGroup({
-    'title': new FormControl(),
-    'price': new FormControl(),
-    'category': new FormControl()
-  });
+  formModel: FormGroup;
+  categories: string[];
+
+  constructor(private productService: ProductService) {
+    this.categories = this.productService.getAllCategories();
+
+    const fb = new FormBuilder();
+    this.formModel = fb.group({
+      'title': [null, Validators.minLength(3)],
+      'price': [null, positiveNumberValidator],
+      'category': [-1]
+    });
+  }
+
+  onSearch() {
+    if (this.formModel.valid) {
+      console.log(this.formModel.value);
+    }
+  }
 }
+
+function positiveNumberValidator(control: FormControl): any {
+  if (!control.value) { return null; }
+  const price = parseInt(null, control.value);
+  return price === null ||
+  typeof price === 'number' &&
+  price > 0 ? null : {positivenumber: true};
+}
+
